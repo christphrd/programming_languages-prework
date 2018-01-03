e69814eb34b063747658948eea01c4458f068016
Christopher Diep <diep.christopher@gmail.com>
2018-01-03 06:47:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,19 +7,10 @@ def reformat_languages(languages)
       new_hash[lang] = {}
       type_data.each do |k,v|
         new_hash[lang][k] = v
+        style_array = []
+        style_array << style
+        new_hash[lang][:style] = style_array
       end
-    end
-  end
-  style_array = []
-  languages.each do |style,lang_list|
-    lang_list.each do |lang,type_data|
-      new_hash[lang][:style] = style_array
-    end
-  end
-  languages.each do |style,lang_list|
-    lang_list.each do |lang,type_data|
-      new_hash[lang][:style] << style
-      new_hash[lang][:style].uniq
     end
   end
   new_hash
